282fdb425ccb169950d36c38b14f8484ef326d62
Elizaveta Zvegintseva <42946813+Elli-zveg@users.noreply.github.com>
2020-07-10 02:29:10 +0300

    Add files via upload

--- src/test/graph/GraphTest.java
+++ src/test/graph/GraphTest.java
@@ -1,61 +1,70 @@
 package test.graph;
 
+import classes.Filer;
 import classes.Log;
 import classes.graph.Graph;
 import classes.graph.NodePlus;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.awt.geom.Point2D;
 
+import static org.junit.Assert.*;
+
 public class GraphTest {
-    public static void runTest() {
-        boolean successful = true;
-        Log.gui(Log.Attributes.BOLD).attr(Log.Attributes.BOLD).say("Тест начался!");
-        Graph graph = new Graph();
 
-        Log.gui().say("Проверяем, пустой ли граф:");
-        successful &= graph.isEmpty();
-        Log.gui().say(graph.isEmpty());
-        if (!graph.isEmpty())
-            Log.gui(Log.Attributes.ITALIC, Log.Attributes.BOLD).say("!Граф не пустой!");
+    Graph graph;
 
-        Log.gui().say("Создаём узел с именем 'Node':");
-        graph.addNode(new NodePlus(new Point2D.Double(0, 0), "Node"));
-        successful &= (graph.getNodes().size() == 1);
-        if (!(graph.getNodes().size() == 1))
-            Log.gui(Log.Attributes.ITALIC, Log.Attributes.BOLD).say("!Узел не создан!");
+    @Before
+    public void start(){
+        //Log.gui(Log.Attributes.BOLD).attr(Log.Attributes.BOLD).col(Log.Colors.GREEN).say("Тест начался!");
+        graph = new Graph();
+        graph.addNode(new NodePlus(new Point2D.Double(200, 400), "A"));
+        graph.addNode(new NodePlus(new Point2D.Double(300, 600), "B"));
+        graph.addNode(new NodePlus(new Point2D.Double(500, 600), "C"));
 
-        Log.gui().say("Создаём узел с именем 'Node1':");
-        graph.addNode(new NodePlus(new Point2D.Double(15, 15), "Node1"));
-        successful &= (graph.getNodes().size() == 2);
-        if (!(graph.getNodes().size() == 2))
-            Log.gui(Log.Attributes.ITALIC, Log.Attributes.BOLD).say("!Узел не создан!");
+        graph.addArk("A", "B", 8);
+        graph.addArk("B", "C", 9);
 
-
-        Log.gui().say("Создаём ребро с весом 7:");
-        graph.addArk("Node","Node1", 7);
-        successful &= (graph.getArks().size() == 1);
-        if (!(graph.getArks().size() == 1))
-            Log.gui(Log.Attributes.ITALIC, Log.Attributes.BOLD).say("!Ребро не создано!");
-
-
-        Log.gui().say("Удаляем ребро с весом 7:");
-        graph.deleteArk("Node","Node1");
-        successful &= (graph.getArks().size() == 0);
-        if (!(graph.getArks().size() == 0))
-            Log.gui(Log.Attributes.ITALIC, Log.Attributes.BOLD).say("!Ребро не удалено!");
-
-        Log.gui().say("Удаляем узел с именем 'Node1':");
-        graph.deleteNode("Node1");
-        successful &= (graph.getNodes().size() == 1);
-        if (!(graph.getNodes().size() == 1))
-            Log.gui(Log.Attributes.ITALIC, Log.Attributes.BOLD).say("!Узел не удален!");
-
-        Log.gui().say("Удаляем узел с именем 'Node':");
-        graph.deleteNode("Node");
-        successful &= (graph.getNodes().size() == 0);
-        if (!(graph.getNodes().size() == 0))
-            Log.gui(Log.Attributes.ITALIC, Log.Attributes.BOLD).say("!Узел не удален!");
-
-        Log.gui(Log.Attributes.BOLD).end("!").say("Тестирование прошло ", successful ? "успешно" : "провально");
     }
+
+    @Test
+    public void addNode() {
+        graph.addNode(new NodePlus(new Point2D.Double(0, 0), "Node"));
+        assertEquals(graph.getNodes().size(), 4);
+    }
+
+    @Test
+    public void deleteNode() {
+
+        graph.deleteNode("A");
+        assertEquals(graph.getNodes().size(), 2);
+    }
+
+    @Test
+    public void changeNode() {
+        graph.changeNode("A", "Q");
+
+    }
+
+    @Test
+    public void addArk() {
+        graph.addArk("A","C", 7);
+        assertEquals(graph.getArks().size(), 3);
+    }
+
+
+    @Test
+    public void deleteArk() {
+        graph.deleteArk("A","B");
+        assertEquals(graph.getArks().size(), 1);
+    }
+
+
+    @Test
+    public void isEmpty() {
+        assertFalse(graph.isEmpty());
+    }
+
+
 }
