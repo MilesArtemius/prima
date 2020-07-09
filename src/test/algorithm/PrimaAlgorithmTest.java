de29d796a9c0eb46ca87d060c7164a5b46261e94
Elizaveta Zvegintseva <42946813+Elli-zveg@users.noreply.github.com>
2020-07-10 02:28:20 +0300

    Add files via upload

--- src/test/algorithm/PrimaAlgorithmTest.java
+++ src/test/algorithm/PrimaAlgorithmTest.java
@@ -1,36 +1,66 @@
 package test.algorithm;
 
-import classes.Log;
-import classes.Settings;
 import classes.algorithm.PrimaAlgorithm;
+import classes.graph.Ark;
 import classes.graph.Graph;
-import classes.shapes.GraphShape;
+import classes.graph.Node;
+import classes.graph.NodePlus;
+import org.junit.Before;
+import org.junit.Test;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.geom.Point2D;
 
-import static classes.Prima.prepareInput;
+import static org.junit.Assert.*;
 
 public class PrimaAlgorithmTest {
-    public static void runTest() {
-        boolean success = true;
 
-        Log.gui(Log.Attributes.BOLD).say("Тест алгоритма начался!");
+    Graph graph;
+    PrimaAlgorithm alg;
 
+    @Before
+    public void start(){
+        graph = new Graph();
+        graph.addNode(new NodePlus(new Point2D.Double(200, 400), "A"));
+        graph.addNode(new NodePlus(new Point2D.Double(300, 600), "B"));
+        graph.addNode(new NodePlus(new Point2D.Double(500, 600), "C"));
 
+        graph.addArk("A", "B", 8);
+        graph.addArk("B", "C", 9);
 
-        Graph graph = prepareInput();
-        GraphShape shape = new GraphShape();
-        shape.setGraph(graph);
-
-        PrimaAlgorithm alg = new PrimaAlgorithm();
-        alg.solve(graph);
-
-        Log.gui().say("Проверяем правильность решения");
-        success &= (alg.CountHidden() == (graph.getNodes().size()-1));
-        if (!(alg.CountHidden() == (graph.getNodes().size()-1)))
-            Log.gui(Log.Attributes.ITALIC,Log.Attributes.BOLD ).say("!Решение не верно!");
-
-        Log.gui(Log.Attributes.BOLD).end("!").say("Тестирование прошло ", success ? "успешно" : "провально");
+        alg = new PrimaAlgorithm();
     }
+
+    @Test
+    public void solve() {
+        alg.solve(graph);
+        assertEquals(alg.CountHidden(),graph.getNodes().size()-1);
+    }
+
+    @Test
+    public void solveStep() {
+        alg.prepareGraph(graph);
+        alg.solveStep();
+        assertNull(alg.solveStep());
+    }
+
+
+    @Test
+    public void prepareGraph() {
+        alg.prepareGraph(graph);
+        int count_ark = 0;
+        int count_node = 0;
+        for (Ark ark: graph.getArks()){
+            if (ark.isHidden()) {
+                count_ark++;
+            }
+        }
+        for (Node node: graph.getNodes()){
+            if (node.isHidden()){
+                count_node++;
+            }
+        }
+        assertEquals(count_ark, 1);
+        assertEquals(count_node, 1);
+    }
+
 }
